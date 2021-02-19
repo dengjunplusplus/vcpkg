36e4203aeb9333c62918e7a2d1f404215cc3c842
DengJun <dengjun@sogou-inc.com>
2021-02-19 16:43:23 +0800

    remove vcpkg-cmake-wrapper.cmake in portfile

--- ports/srpc/portfile.cmake
+++ ports/srpc/portfile.cmake
@@ -30,4 +30,3 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
 vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/srpc RENAME copyright)
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/srpc")
